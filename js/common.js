d6259d22a643d7ef628d15b49fe63816db49334d
etna-scorpio <etna.scorpio@gmail.com>
2015-04-30 16:54:34 +0300

    fixes

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
 head.ready(function() {
 
+	//truncate paragraph
 	$(".item__desc").dotdotdot({
 		ellipsis: "..."
 	});
+
 
 	//on resize
 	$(window).resize(function(){
@@ -11,6 +13,58 @@ head.ready(function() {
 			ellipsis: "..."
 		});
 
+		scrollSidebar();
+		footerTop();
+		footerHeight();
+
 	});
+
+	$(window).scroll(function(){
+		scrollSidebar();
+	});
+
+	//fixed sidebar
+	function scrollSidebar(){
+		if($('.sidebar').length > 0){
+			var width = $(window).width();
+			var scroll = $(window).scrollTop();
+			var sidebar = $('.js-sidebar');
+			var sidebarHeight = $('.sidebar').height();
+			var sidebarPosition = $('.sidebar').offset().top;
+
+			if(scroll >= sidebarPosition && width > 751){
+				sidebar.addClass('is-fixed');
+				sidebar.css({'width': sidebarWidth()});
+			} else {
+				sidebar.removeClass('is-fixed');
+				sidebar.css('width', sidebarWidth());
+			}
+			console.log(scroll);
+		}
+	}
+	scrollSidebar();
+
+	//measuring width of sidebar wrapper
+	function sidebarWidth(){
+
+		var sidebarWidth = $('.sidebar').width();
+		return sidebarWidth;
+
+	}
+
+	//measure stop point
+	function footerTop(){
+		var footerTop = $('.footer').offset().top;
+		return footerTop;
+	}
+
+	footerTop();
+
+	function footerHeight(){
+		var footerHeight = $('.footer').height();
+		return footerHeight;
+	}
+	footerHeight();
+	console.log(footerTop() - footerHeight());
 
 });
